aa728174a43f01b116d4fe0cedb7330d62935093
loganself <forrestlogan.self@gmail.com>
2019-05-15 13:56:34 -0500

    no idea

--- finalproject/scripts.js
+++ finalproject/scripts.js
@@ -59,7 +59,7 @@ Plotly.d3.csv('abia.csv', function(err, rows){
           type: 'scattermapbox',
           lat: unpack(rows, 'Latitude'),
           lon: unpack(rows, 'Longitude'),
-          text: unpack(rows, 'airports', 'passengers'),
+          text: unpack(rows, 'airports, passengers'),
           marker: {
             color: ['#000']
           }
